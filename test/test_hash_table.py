2aa94bb64646cb6dd886f12a60482a7dfa8c64d8
blecharczyk <k.blecharczyk@onet.pl>
2023-05-26 21:28:47 +0200

    Implement HashTable::get_size() - red phase

--- test/test_hash_table.py
+++ test/test_hash_table.py
@@ -1,6 +1,15 @@
 import pytest
 
-class TestHashTable:
+from src.hash_table import HashTable
 
-    def test_true(self):
-        assert 0 == 0
+
+class TestHashTable:
+    def test_create_hash_table_with_given_size_and_check_size(self):
+        # given
+        hash_table_size = 5
+
+        # when
+        hash_table = HashTable(hash_table_size)
+
+        # then
+        assert hash_table.get_size() == hash_table_size
